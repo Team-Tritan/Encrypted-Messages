73dac78df449379304ddabb60eae18c152218c30
Dylan <dylan@tritan.dev>
2024-01-23 04:55:34 -0500

    a lil better styling

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,7 @@ interface Secret {
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "pages");
 
